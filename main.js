2bc049b584c140220bed64d30ade42caad64b020
j-goodman <johngoodman@pursuit.org>
2023-03-19 12:14:27 -0400

    Initial commit.

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+let colorButtons = document.querySelectorAll(".color")
+let currentColorSquare = document.querySelector("#current-color")
+
+for (const colorButton of colorButtons) {
+  colorButton.addEventListener("click", (event) => {
+    currentColorSquare.style.background = event.target.style.background
+  })
+}
+
+let colorCells = document.querySelectorAll(".cell")
+
+colorCells.forEach(cell => {
+  cell.addEventListener("click", (event) => {
+    cell.style.background = currentColorSquare.style.background
+  })
+})
